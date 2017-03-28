ff46c941b1b7b018753ef2108371a17be11484d5
evans <evansjwang@gmail.com>
2017-03-28 11:56:49 -0400

    Display user input in code

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,9 +2,10 @@ puts "What's the hamster name?"
 hamster_name = gets.chomp
 puts "On a scale of 1 to 10 how loud is #{hamster_name}?"
 loudness = gets.chomp
+loudness.to_i
 puts "What color is #{hamster_name}?"
 color = gets.chomp
-puts "Would you take #{hamster_name} home? (y/n)"
+puts "Would you take #{hamster_name} home? (yes/no)"
 adopability = gets.chomp
 puts "How old do you think homeboy is?"
 age = gets.chomp
@@ -16,5 +17,8 @@ else
   age = nil
 end
 
-puts "#{age}"
-
+puts "Name is #{hamster_name}"
+puts "loud facter = #{loudness}"
+p "Hamster color is #{color}"
+p "Is hamster super awesome? #{adopability}"
+puts "Age is #{age}"
