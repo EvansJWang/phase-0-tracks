1818bf97f358ba0b39d95c5db2ba48c27f5a127b
evans <evansjwang@gmail.com>
2017-03-28 13:53:05 -0400

    Create vampire questionaire

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+puts "What's your name?"
+name = gets.chomp
+p "How old are you?"
+age = gets.chomp
+age.to_i
+p "What year were you born?"
+birthyear = gets.chomp
+birthyear.to_i
+p "Our company cafeteria serves garlic bread. Should we order some for you? "
+garlicbread = gets.chomp
+p "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
