2f96953297e119dbafd8116fb0c63f980083ed1c
evans <evansjwang@gmail.com>
2017-03-29 18:02:55 -0400

    Add loop for numerous employee processing

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,21 +1,46 @@
-puts "What's your name?"
-name = gets.chomp
-p "How old are you?"
-age = gets.chomp.to_i
-p "What year were you born?"
-birthyear = gets.chomp.to_i
-p "Our company cafeteria serves garlic bread. Should we order some for you? "
-garlicbread = gets.chomp
-p "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
 
+  
+  puts "how many employees will be proceesed  ?"
+  numemployees = gets.chomp.to_i
 
-  if age == (2017 - birthyear)
-    vampstatus = true
-    puts "Probably not a vampire"
-  else 
-    puts "Definitely a vampire"
-  end
+ 
+until numemployees == 0
+    numemployees -= 1
 
+  
+  puts "What's your name?"
+  name = gets.chomp
+  p "How old are you?"
+  age = gets.chomp.to_i
+  p "What year were you born?"
+  birthyear = gets.chomp.to_i
+  p "Our company cafeteria serves garlic   bread. Should we order some for you? "
+  garlicbread = gets.chomp
+  p "Would you like to enroll in the   company's health insurance?"
+  insurance = gets.chomp
+  
+  
 
-#&& (#{garlicbread} == yes && #{insurance}  == yes)
+  
+      if
+        garlicbread === "yes" && insurance   === "yes" && age == (2017 -   birthyear) 
+        vampstatus = true
+        puts "Probably not a vampire"
+      elsif     age == (2017 - birthyear) &&     (garlicbread == "no" || insurance   ==   "no")
+      vampstatus = true
+        puts "Probably a vampire"
+      elsif name =="Drake Cula"
+      vampstatus = true
+      puts "Definitely a vampire"
+      elsif name == "Tu Fang"
+      vampstatus = true
+      puts "Definitely a vampire"
+      else
+        puts "Results inconclusive"
+        vampstatus=true
+      end
+end
+#end  -- placeholder for until 
+
+  puts "Actually, nevermind! What do these questions have to do with anything? Let's all be friends."
+
