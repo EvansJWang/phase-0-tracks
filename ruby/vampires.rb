ff5f1a995e433f4a1a545623e3d78ccd832e6066
evans <evansjwang@gmail.com>
2017-03-29 11:54:52 -0400

    Add first vampire detection question

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,12 +1,21 @@
 puts "What's your name?"
 name = gets.chomp
 p "How old are you?"
-age = gets.chomp
-age.to_i
+age = gets.chomp.to_i
 p "What year were you born?"
-birthyear = gets.chomp
-birthyear.to_i
+birthyear = gets.chomp.to_i
 p "Our company cafeteria serves garlic bread. Should we order some for you? "
 garlicbread = gets.chomp
 p "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
+
+
+  if age == (2017 - birthyear)
+    vampstatus = true
+    puts "Probably not a vampire"
+  else 
+    puts "Definitely a vampire"
+  end
+
+
+#&& (#{garlicbread} == yes && #{insurance}  == yes)
