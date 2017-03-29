9a574ac6b77c136a04f39b6288f3ace9d63fa4df
evans <evansjwang@gmail.com>
2017-03-29 18:17:21 -0400

    Add allergy questionaire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -39,8 +39,29 @@ until numemployees == 0
         puts "Results inconclusive"
         vampstatus=true
       end
-end
-#end  -- placeholder for until 
 
+
+allergies = FALSE
+until allergies 
+
+p "What are you allergic to?"
+allergy = gets.chomp
+
+  if allergy == "nothing"
+    allergies = TRUE
+    p "Awesome"
+  elsif allergy == "sunshine"
+    allergies = TRUE
+    p "Probably a vampire"
+  elsif 
+   allergy == "done"
+   allergies = TRUE
+   puts "Thanks for the allergy list"
+  else 
+    puts "Any other allergies? If not then type 'done' "
+  end
+  
+end
+end
   puts "Actually, nevermind! What do these questions have to do with anything? Let's all be friends."
 
