5b738960fecb2146a1b7881e145d7a5e3ce56671
evans <evansjwang@gmail.com>
2017-04-12 19:53:05 -0400

    Create pseudocode for release 0 GPS2.2

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,42 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # 1. Get string, 
+  # 2. spilt into words, 
+  # 3. for each word, each value == Key
+  # 4. set default value = 1 
+  # # set default quantity
+  # # print the list to the console [can you use one of your other methods here?]
+  # 5. call print list method - displays list to console
+# output: List = Hash Item = Key Quantity = value
+
+# Method to add an item to a list
+# # input: list, item name, and optional quantity
+# # steps:
+#   1. print list method to visualize so far
+#   2. create a method that takes 3 values (list, item name(key), quantity(value))
+# # output:  list { item => quantity }
+
+# Method to remove an item from the list
+# input: list and item to delete from list
+# steps:
+  1. print list method to visualize
+  2. create a method takes 2 inputs (list, item_to_delete)
+  3. delete that item from list 
+# output: List { item => quantity } *without item_to_delete
+
+# Method to update the quantity of an item. 
+# input:  
+# steps:
+1. print the list 
+2. method takes 3 values ( list, item_name, quantity)
+# output: list { item => quantity updated}
+
+# Method to print a list and make it look pretty
+# input: list
+# steps: 
+1. prints "--------"
+2. prints " shopping list" (keys)
+3. prints " quantity" (values)
+4. prints "--------"
+# output: 
