6afd5df1d505daf664843248415cb187c2d06871
evans <evansjwang@gmail.com>
2017-04-18 12:02:10 -0400

    Create algorithm in JS to reverse a string

---
+++ js/explore.js
@@ -0,0 +1,15 @@
+// Create a function that takes a string and reverses it
+// INPUT reverse("hello") return OUTPUT "olleh"
+// Set a variable to test
+// For the length of the word do the following:
+// while i (index) is greater than 0 
+// count index down (in order to go backwards)
+// print word from highest index(last letter) to 0 (first letter)
+
+
+var word = "example";
+for ( var i = word.length; i > 0; i-=1) {
+  backwardsWord = console.log(word[i-1])
+}
+;
+
