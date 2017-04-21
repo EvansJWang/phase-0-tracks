2b8034fb3586a81bd1b25e6819ae301eb9eafec8
evans <evansjwang@gmail.com>
2017-04-21 17:31:07 -0400

    Complete method that makes random array

--- js/algos.js
+++ js/algos.js
@@ -17,3 +17,50 @@ function lengthChecker(arr) {
     }
      return longestWord;
 }
+
+
+// // Create a function that will search the first object's keys with all the keys of the second object 
+
+var example = {name: "Steven", age: 54};
+var example1 = {name: "Tamir", age: 54};
+
+function keyValueMatch(object1, object2) {
+  var match = false
+  for (var i = 0; i < Object.keys(object1).length; i++ ) {
+      for (var ii = 0; ii < Object.keys(object2).length; ii++) {
+      if (Object.keys(object1)[i] == Object.keys(object2)[ii]) {match = true};
+      return match
+      }
+  }    
+}
+
+
+// console.log(Object.keys(example1).length)
+// console.log(Object.keys(example1))
+// console.log(Object.keys(example1)[0]);
+// match(example,example1);
+// console.log(Object.keys(example1))
+
+
+// create a function that takes a random letter a random number of times and makes it a word 
+// store that word into an array a certain number of times
+
+function makeArray(numberOfTimes){
+  var array = [];
+  for (var i = 0; i < numberOfTimes; i++) {
+    var randomString = "";
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    var randomNum = Math.floor((Math.random() * 10) + 1);
+      for (var ii = 0; ii < Math.floor((Math.random() * 100) + 1); ii+=5) {
+         randomString += alphabet[Math.floor((Math.random() * 25) + 1)];}
+    array[i] = randomString;
+    }
+    return array
+  }
+// create a random array with function makeArray
+// print it  then feed it into lengthChecker method
+  var doublecheck = makeArray(10)
+  console.log(doublecheck)
+  console.log(lengthChecker(doublecheck))
+
+
