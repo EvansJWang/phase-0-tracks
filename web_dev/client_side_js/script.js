0afd42c08e899b646f256de8fde8977972ecfbcf
evans <evansjwang@gmail.com>
2017-05-02 12:05:22 -0400

    Release 1

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,25 @@
+console.log("word! $$$$");
+//Add dotted blue border around one <em> node
+var ems = document.getElementsByTagName("em");
+ems[0]
+em = ems[0]
+em.style.border = "2px dotted blue"
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var newHeading = document.createElement('h3');
+// var h3Text = document.createTextNode('does this work?');
+// newHeading.appendChild(h3Text);
+// parent.insertBefore('h2');
+
+// <h3>does this work?</h3>
