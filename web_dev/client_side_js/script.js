f4e718be2c846efcd62273f2bb94ab24469f6d25
evans <evansjwang@gmail.com>
2017-05-02 12:55:52 -0400

    Release 2

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,12 +1,19 @@
 console.log("word! $$$$");
 //Add dotted blue border around one <em> node
 var ems = document.getElementsByTagName("em");
-ems[0]
-em = ems[0]
-em.style.border = "2px dotted blue"
+ems[0];
+em = ems[0];
+em.style.border = "2px dotted blue";
 
 
+function fireworksButton() {
+  console.log ("BAM!");
 
+};
+
+
+var button = document.getElementsByTagName("button");
+button[0].addEventListener("click", fireworksButton);
 
 
 
